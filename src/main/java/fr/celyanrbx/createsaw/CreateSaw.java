b590957aa001d9587ad4d5dd6d17b60a75887258
CelyanRbx <glmtjulie@gmail.com>
2025-06-10 00:06:20 +0200

    first commit

---
+++ src/main/java/fr/celyanrbx/createsaw/CreateSaw.java
@@ -0,0 +1,12 @@
+package fr.celyanrbx.createsaw;
+
+import org.slf4j.Logger;
+import com.mojang.logging.LogUtils;
+import net.neoforged.fml.common.Mod;
+
+@Mod(CreateSaw.MODID)
+public class CreateSaw {
+
+    public static final String MODID = "createsaw";
+    private static final Logger LOGGER = LogUtils.getLogger();
+}
